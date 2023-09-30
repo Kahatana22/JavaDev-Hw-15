f64723a97ebc24e6e002a1ba48c3391ada37911e
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-09-30 16:17:59 +0300

    Create JavaDevHw15ApplicationTests.java

---
+++ src/test/java/com/example/JavaDevHw15/JavaDevHw15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaDevHw15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevHw15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

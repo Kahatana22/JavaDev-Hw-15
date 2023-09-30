89935723deec49ac6935adfca11f13df7873c362
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-09-30 16:17:33 +0300

    Create NoteService.java

---
+++ src/main/java/com/example/JavaDevHw15/services/NoteService.java
@@ -0,0 +1,46 @@
+package com.example.JavaDevHw15.services;
+
+import com.example.JavaDevHw15.entities.Note;
+import com.example.JavaDevHw15.repositories.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    @Autowired
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public List<Note> listAll() {
+        return (List<Note>) noteRepository.findAll();
+    }
+
+    public Note add(Note note) {
+        if (!Objects.isNull(note)) {
+            return noteRepository.save(note);
+        }
+        return null;
+    }
+
+    public void deleteById(Long id) {
+        noteRepository.deleteById(id);
+    }
+
+    public void update(Note note) {
+        Note currentNote = noteRepository.findById(note.getId()).get();
+        currentNote.setTitle(note.getTitle());
+        currentNote.setContent(note.getContent());
+        noteRepository.save(currentNote);
+    }
+
+    public Optional<Note> getById(Long id) {
+        return noteRepository.findById(id);
+    }
+}

e9e342760c9df57b7f2609af928be279eb21b355
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-09-30 16:17:23 +0300

    Create JavaDevHw15Application.java

---
+++ src/main/java/com/example/JavaDevHw15/JavaDevHw15Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaDevHw15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevHw15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevHw15Application.class, args);
+	}
+
+}
